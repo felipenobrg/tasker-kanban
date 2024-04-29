7cf26e7fbd805906a37d1ba3aae74b43b3a6cee9
Perazzojoao <perazzojoao@gmail.com>
2024-04-29 19:09:12 -0300

    Adding new v2 api for production

---
+++ backend/v2/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"tasker/database"
+	"tasker/routes"
+)
+
+const webPort = "8080"
+
+func main() {
+	// Connect to database
+	postgresConn, err := database.Connect()
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+
+	app := routes.Config{
+		Postgres: postgresConn,
+	}
+	log.Printf("Server is running on port:%s\n\n", webPort)
+
+	// Define http server
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.Routes(),
+	}
+
+	// Start server
+	if err := srv.ListenAndServe(); err != nil {
+		log.Fatal(err)
+	}
+}

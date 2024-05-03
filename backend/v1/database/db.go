1a6af147f2a110bb17a858d33e167c3ba0e16552
Perazzojoao <perazzojoao@gmail.com>
2024-05-03 01:19:10 -0300

    Adding subTasks table into database

--- backend/v1/database/db.go
+++ backend/v1/database/db.go
@@ -12,7 +12,6 @@ import (
 	"gorm.io/gorm"
 
 	"tasker/models"
-
 )
 
 var (
@@ -47,7 +46,7 @@ func Connect() (*gorm.DB, error) {
 			counts++
 		} else {
 			log.Println("Conectado com postgres")
-			DB.AutoMigrate(&models.User{}, &models.Board{}, &models.Task{})
+			DB.AutoMigrate(&models.User{}, &models.Board{}, &models.Task{}, &models.SubTask{})
 			connection = DB
 			break
 		}

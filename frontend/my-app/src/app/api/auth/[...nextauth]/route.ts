b2586ec5e851c3d05cd41dac1026df1363c83440
felipenobrg <felipenobrega2012@gmail.com>
2024-05-17 19:51:16 -0300

    fixing auth error

--- frontend/my-app/src/app/api/auth/[...nextauth]/route.ts
+++ frontend/my-app/src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,5 @@
 import NextAuth, { NextAuthOptions, Session } from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
-import { NextApiRequest, NextApiResponse } from 'next';
 import { BASE_URL } from '../../../../../apiConfig';
 
 interface CustomUser {
@@ -8,66 +7,55 @@ interface CustomUser {
   email?: string;
 }
 
-export default async function auth(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'POST') {
-    const authOptions: NextAuthOptions = {
-      providers: [
-        CredentialsProvider({
-          name: 'Credentials',
-          credentials: {
-            email: { label: 'Email', type: 'text' },
-            password: { label: 'Password', type: 'password' },
-          },
-          async authorize(credentials) {
-            try {
-              const response = await fetch(`${BASE_URL}/login`, {
-                method: 'POST',
-                headers: {
-                  'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(credentials),
-              });
-
-              const user = await response.json();
-              if (response.ok && user) {
-                return user;
-              }
-
-              return null;
-            } catch (error) {
-              console.error('Authorization Error:', error);
-              return null;
-            }
-          },
-        }),
-      ],
-      pages: {
-        signIn: '/login',
-        signOut: '/login',
+const handler = NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: { label: 'Email', type: 'text' },
+        password: { label: 'Password', type: 'password' },
       },
-      callbacks: {
-        async jwt({ token, user }) {
-          if (user) {
-            token.user = user as CustomUser;
+      async authorize(credentials) {
+        try {
+          const response = await fetch(`${BASE_URL}/login`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(credentials),
+          });
+
+          const user = await response.json();
+          if (response.ok && user) {
+            return user;
           }
-          return token;
-        },
-        async session({ session, token }): Promise<Session> {
-          session.user = token.user as CustomUser;
-          return session;
-        },
+
+          return null;
+        } catch (error) {
+          console.error('Authorization Error:', error);
+          return null;
+        }
       },
-    };
-
-    await NextAuth(req, res, authOptions);
-  } else {
-    res.setHeader('Allow', ['POST']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
-  }
-}
-
-export const nextAuthConfig = {
-  api: {
-    bodyParser: false,
+    }),
+  ],
+  pages: {
+    signIn: '/login',
+    signOut: '/login',
   },
-};
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.user = user as CustomUser;
+      }
+      return token;
+    },
+    async session({ session, token }): Promise<Session> {
+      session.user = token.user as CustomUser;
+      return session;
+    },
+  },
+}
+)
+
+export { handler as GET, handler as POST}
+
